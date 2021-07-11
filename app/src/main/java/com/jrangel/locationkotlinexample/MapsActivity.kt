ede77072e80f318f2c2f809cdd0eee6a4fb5186f
Josue <josrangel@hotmail.com>
2021-07-11 02:45:01 -0500

    - add MapsActivity - modify MapsActivity for show latitude and longitude in the map

---
+++ app/src/main/java/com/jrangel/locationkotlinexample/MapsActivity.kt
@@ -0,0 +1,53 @@
+package com.jrangel.locationkotlinexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.jrangel.locationkotlinexample.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private var latitude = 0.0
+    private var longitude = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val extras = intent.extras
+        latitude = extras?.getDouble("latitude",0.0) ?: 0.0
+        longitude = extras?.getDouble("longitude",0.0) ?: 0.0
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     *
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val positionNow = LatLng(latitude, longitude)
+        mMap.addMarker(MarkerOptions().position(positionNow).title("Position Actual"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(positionNow,12.0f))
+    }
+}
